b7d962128a4b4a53a94ea4655608b7474fb77a40
Tansen <aatansen@gmail.com>
2022-03-16 00:42:03 +0600

    renamed

--- 01 extra/generate random colors.py
+++
@@ -1,8 +0,0 @@
-import random
-randomcolors = []
-colours=['blue','brown','green','red','yellow','orange','black','white']
-# print(colours[0])
-for i in range(0, 200):
-    n = random.randint(0, 7)
-    randomcolors.append(colours[n])
-print(randomcolors)
